aecc1151d6f4c95f439f90cd86fc5102212f5e8e
VishaliKadali <kvishalicse@gmail.com>
2025-08-12 23:10:18 +0530

    Day10_TestDescribe_mode_SerialorParallel_Retries

--- tests/day10/TestDescribe.spec.ts
+++ tests/day10/TestDescribe.spec.ts
@@ -1,6 +1,6 @@
 import test from '@playwright/test'
 test.use({storageState:"utils/LoginStorage.json"})
-test.describe.parallel("Test Annotation Describe",()=>{
+test.describe.serial("Test Annotation Describe",()=>{
 
     test("Click Accounts", async({page})=>{
         await page.goto("http://leaftaps.com/crmsfa/control/main")
@@ -9,6 +9,23 @@ test.describe.parallel("Test Annotation Describe",()=>{
     })
 
     test("Click Leads", async({page})=>{
+        await page.goto("http://leaftaps.com/crmsfa/control/main")
+        await page.locator("//a[text()='Leads']").click()
+        console.log(await page.title())
+    })
+})
+
+test.use({storageState:"utils/LoginStorage.json"})
+test.describe("Test Annotation Describe",()=>{
+
+    test.describe.configure({mode:"serial", retries:1})
+    test("Click Accounts config", async({page})=>{
+        await page.goto("http://leaftaps.com/crmsfa/control/main")
+        await page.click("//a[text()='Accounts']")
+        console.log(await page.title())
+    })
+
+    test("Click Leads config", async({page})=>{
         await page.goto("http://leaftaps.com/crmsfa/control/main")
         await page.locator("//a[text()='Leads']").click()
         console.log(await page.title())
