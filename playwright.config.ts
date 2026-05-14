09d99540f92b005c15566d1fe5cadc67f5d48099
VishaliKadali <kvishalicse@gmail.com>
2026-05-14 16:44:22 +0530

    Updated Playwright config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
-  headless:false,
+  headless:true,
   video: 'off',
   screenshot: 'on',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
@@ -44,18 +44,29 @@ export default defineConfig({
   },
 
   /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: {'browserName':'chromium', channel:'chrome',
-        viewport:null,
-        launchOptions:{
-          args:['--start-maximized'],
+  // projects: [
+  //   {
+  //     name: 'chromium',
+  //     use: {'browserName':'chromium', channel:'chrome',
+  //       viewport:null,
+  //       launchOptions:{
+  //         args:['--start-maximized'],
           
-        }
-      },
-       testDir: './tests',
+  //       }
+  //     },
+  //      testDir: './tests',
+  //   },
+
+  //githubactions 
+  projects: [
+  {
+    name: 'chromium',
+    use: {
+      ...devices['Desktop Chrome'],
     },
+    testDir: './tests',
+  },
+],
     
     // {
     //   name: 'chromium',
@@ -93,7 +104,7 @@ export default defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  ],
+  //],
 
   /* Run your local dev server before starting the tests */
   // webServer: {
